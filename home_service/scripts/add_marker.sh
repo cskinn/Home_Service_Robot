483da52fed3fb12f138b190763ab581bb5bbcaf5
cskinn <162724394+cskinn@users.noreply.github.com>
2026-08-25 19:47:05 -0500

    Update add_marker.sh
    removing turtlebot roots

--- home_service/scripts/add_marker.sh
+++ home_service/scripts/add_marker.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cd ../..
+cd ../../..
 
 source devel/setup.bash
 
 #Launch the turtlebot world
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "roslaunch home_service world.launch" &
 
 # Wait for a few seconds to ensure the world is loaded
 sleep 5
 
 # Launch the gmapping demo
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch home_service amcl_demo.launch" &
 
 # Wait for a few seconds to ensure gmapping is loaded
 sleep 5
 
 # Launch RViz to visualize the map
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch home_service view_navigation.launch" &
 
 sleep 5
 
